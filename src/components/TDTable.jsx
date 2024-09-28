6a88b9cf579b1c5de767fb321c28953ec9a42bd3
Nandan Devadula <devadula.nandan@gmail.com>
2024-09-28 20:44:36 +0530

    fix

--- src/components/TDTable.jsx
+++ src/components/TDTable.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import React, { useState } from 'react';
 import { flexRender } from '@tanstack/react-table';
 import './TDTable.css';
 import PaginationControls from './PaginationControls';
