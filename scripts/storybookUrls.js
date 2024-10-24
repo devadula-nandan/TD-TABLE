bde0b0bb75467ab5aa8b84ed3ab349d39322a7e6
Nandan Devadula <devadula.nandan@gmail.com>
2024-10-24 18:02:43 +0530

    dynamic storybook urls generation

---
+++ scripts/storybookUrls.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+const storybookStaticDir = path.resolve(__dirname, "../storybook-static");
+const indexPath = path.join(storybookStaticDir, "index.html");
+const baseUrl = "http://localhost:6006"; // or the URL your Storybook is served from
+
+if (fs.existsSync(indexPath)) {
+  const indexHtml = fs.readFileSync(indexPath, "utf8");
+  const storyRegex = /<a href="\/(.*?)"/g;
+  const storyUrls = [];
+  let match;
+
+  while ((match = storyRegex.exec(indexHtml)) !== null) {
+    storyUrls.push(`${baseUrl}/${match[1]}`);
+  }
+
+  fs.writeFileSync("storybook-urls.txt", storyUrls.join("\n"));
+  console.log("Storybook URLs saved to storybook-urls.txt");
+} else {
+  console.error("Storybook index.html not found!");
+  process.exit(1);
+}
